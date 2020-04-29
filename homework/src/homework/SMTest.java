4f7189dd2861a12e042b88d621e459a48ba84864
pengxueqing <2281051864@qq.com>
2020-04-29 10:33:07 +0800

    第一次提交

---
+++ homework/src/homework/SMTest.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class SMTest {
+	public static void main(String[] args) {
+		StudentManager sm = new StudentManager();
+		sm.inite();
+		sm.app();
+//		sm.insert();
+	}
+}
